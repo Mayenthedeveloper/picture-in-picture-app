0ac5ee964ba54cdfef68a05ae022f584bc1d6109
Mayen Akpan <neroxma@yahoo.com>
2022-01-15 21:56:58 -0800

    updated

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Promt user to select meedia stream, pass to  video ele and play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    //catch error
+    console.log("whoops, error here", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  //disable button when
+  button.diabled = true;
+  //start picture in picture
+  await videoElement.requestPictureInPicture();
+  //reset button
+  button.diabled = false;
+});
+
+//on load
+selectMediaStream();
